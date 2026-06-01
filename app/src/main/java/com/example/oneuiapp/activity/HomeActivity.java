42dde070230831f1d7359ca7e1f5130a2247c522
tamimi30 <mohamad.tamimi6.82@gmail.com>
2026-06-01 17:56:16 +0300

    Refactor HomeActivity by removing toolbar code
    Removed toolbar initialization and related methods from HomeActivity.

--- app/src/main/java/com/example/oneuiapp/activity/HomeActivity.java
+++ app/src/main/java/com/example/oneuiapp/activity/HomeActivity.java
@@ -2,26 +2,12 @@ package com.example.oneuiapp.activity;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-
-import androidx.annotation.NonNull;
 
 import com.example.oneuiapp.R;
 import com.example.oneuiapp.fragment.HomeFragment;
 
-import dev.oneuiproject.oneui.layout.ToolbarLayout;
-
-/**
- * HomeActivity - شاشة الصفحة الرئيسية المستقلة
- * 
- * تعرض HomeFragment في شاشة منفصلة مع شريط أدوات علوي غير قابل للتوسع
- * تم إنشاؤها لحل مشكلة تعارض CollapsingToolbar المزدوج
- * مصممة بنفس أسلوب SettingsActivity
- */
 public class HomeActivity extends BaseActivity {
 
-    private ToolbarLayout mToolbarLayout;
     private HomeFragment mHomeFragment;
 
     @Override
@@ -29,8 +15,6 @@ public class HomeActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        initToolbar();
-        
         if (savedInstanceState == null) {
             mHomeFragment = new HomeFragment();
             getSupportFragmentManager()
@@ -41,28 +25,6 @@ public class HomeActivity extends BaseActivity {
             mHomeFragment = (HomeFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.home_container);
         }
-    }
-
-    private void initToolbar() {
-        mToolbarLayout = findViewById(R.id.toolbar_layout);
-        if (mToolbarLayout != null) {
-            mToolbarLayout.setNavigationButtonTooltip(getString(R.string.navigate_up));
-            mToolbarLayout.setNavigationButtonOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    onBackPressed();
-                }
-            });
-        }
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            onBackPressed();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
