2488d5899979450ef6ffb4a7804d9815e04da4f2
tamimi30 <mohamad.tamimi6.82@gmail.com>
2026-06-04 06:26:57 +0300

    Comment out FloatingActionButton import
    Comment out the import statement for FloatingActionButton.

--- app/src/main/java/com/example/oneuiapp/activity/MainActivity.java
+++ app/src/main/java/com/example/oneuiapp/activity/MainActivity.java
@@ -19,7 +19,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.appcompat.app.AlertDialog;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+//import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 
 
