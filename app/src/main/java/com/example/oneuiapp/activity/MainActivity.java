37d07fa62e32d50521e049753def603333d38f9d
tamimi30 <mohamad.tamimi6.82@gmail.com>
2026-06-04 15:03:24 +0300

    Fix drawable creation for fabFontSize

--- app/src/main/java/com/example/oneuiapp/activity/MainActivity.java
+++ app/src/main/java/com/example/oneuiapp/activity/MainActivity.java
@@ -926,7 +926,7 @@ public class MainActivity extends BaseActivity
             // جلب لون النص الخاص بثيم OneUI
             int textColor = getColor(dev.oneuiproject.oneui.design.R.color.oui_primary_text_color);
             // رسم الرقم بحجم 60 ووضعه كصورة داخل الزر العادي
-            fabFontSize.setImageDrawable(new (String.valueOf(Math.round(size)), 24f, textColor));
+            fabFontSize.setImageDrawable(new TextDrawable(String.valueOf(Math.round(size)), 24f, textColor));
         }
     }
 
