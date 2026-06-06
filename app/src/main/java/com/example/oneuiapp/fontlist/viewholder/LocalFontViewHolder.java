ea1bae812229324c12b5edea3fb13211cd255eb0
tamimi30 <mohamad.tamimi6.82@gmail.com>
2026-06-07 00:38:46 +0300

    Add condition to updateLastOpenedHighlight method

--- app/src/main/java/com/example/oneuiapp/fontlist/viewholder/LocalFontViewHolder.java
+++ app/src/main/java/com/example/oneuiapp/fontlist/viewholder/LocalFontViewHolder.java
@@ -305,7 +305,7 @@ public class LocalFontViewHolder extends RecyclerView.ViewHolder {
      */
     public void updateLastOpenedHighlight(boolean isLastOpened) {
         Context context = fontNameTextView.getContext();
-        if (isLastOpened) {
+        if (isLastOpened && !isSearchActive) {
             int primaryColor = MaterialColors.getColor(
                 context,
                 androidx.appcompat.R.attr.colorPrimary,
