b19105085a64e5f2fe6386414ba638ba4257d7f3
tamimi30 <mohamad.tamimi6.82@gmail.com>
2026-06-11 06:28:02 +0300

    Add Log import to SelectableLinearLayout

--- app/src/main/java/com/example/oneuiapp/widget/SelectableLinearLayout.java
+++ app/src/main/java/com/example/oneuiapp/widget/SelectableLinearLayout.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.util.Log;
 
 import androidx.appcompat.widget.AppCompatCheckBox;
 
