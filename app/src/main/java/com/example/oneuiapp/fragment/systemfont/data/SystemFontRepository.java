4d9cfc4272d3a6fde7dc4ca2216d159a94347920
tamimi30 <mohamad.tamimi6.82@gmail.com>
2026-06-15 14:17:22 +0300

    Refactor package structure for SystemFontRepository

--- app/src/main/java/com/example/oneuiapp/fragment/systemfont/data/SystemFontRepository.java
+++ app/src/main/java/com/example/oneuiapp/fragment/systemfont/data/SystemFontRepository.java
@@ -1,4 +1,4 @@
-package com.example.oneuiapp.fragment.systemfont.data.repository;
+package com.example.oneuiapp.fragment.systemfont.data;
 
 import android.content.Context;
 import android.util.Log;
