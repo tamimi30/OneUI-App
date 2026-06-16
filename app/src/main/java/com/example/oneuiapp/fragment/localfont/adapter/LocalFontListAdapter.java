fe7acff6c6f63bed3bf0c1932e2ddfa239d38b16
tamimi30 <mohamad.tamimi6.82@gmail.com>
2026-06-16 14:23:59 +0300

    Update import statement for FontTextHighlighter

--- app/src/main/java/com/example/oneuiapp/fragment/localfont/adapter/LocalFontListAdapter.java
+++ app/src/main/java/com/example/oneuiapp/fragment/localfont/adapter/LocalFontListAdapter.java
@@ -20,7 +20,7 @@ import com.example.oneuiapp.R;
 import com.example.oneuiapp.fontlist.FontFileInfo;
 import com.example.oneuiapp.fragment.localfont.data.LocalFontCache;
 import com.example.oneuiapp.fragment.localfont.manager.LocalFontPreferenceManager;
-import com.example.oneuiapp.fontlist.search.FontTextHighlighter;
+import com.example.oneuiapp.search.FontTextHighlighter;
 import com.example.oneuiapp.fragment.localfont.adapter.LocalFontViewHolder;
 import com.example.oneuiapp.sort.SortHeaderViewHolder;
 import com.example.oneuiapp.metadata.FontWeightWidthExtractor;
