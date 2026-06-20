530ab496a14a7ef3b8816b028a20bac85d13add2
tamimi30 <mohamad.tamimi6.82@gmail.com>
2026-06-20 15:40:53 +0300

    Change postDelayed delay from 100ms to 0ms

--- lib/src/main/java/dev/oneuiproject/oneui/layout/ToolbarLayout.java
+++ lib/src/main/java/dev/oneuiproject/oneui/layout/ToolbarLayout.java
@@ -1017,7 +1017,7 @@ public class ToolbarLayout extends LinearLayout {
                         updateActionModeMenuVisibility(mContext.getResources().getConfiguration());
                     }
                 };
-                postDelayed(mTitleUpdateRunnable, 100);
+                postDelayed(mTitleUpdateRunnable, 0);
             } else {
                 mCollapsingToolbarLayout.setTitle(title);
                 mActionModeTitleTextView.setText(title);
