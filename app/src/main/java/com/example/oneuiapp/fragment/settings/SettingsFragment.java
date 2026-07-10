e46f01d046e5f7ef841d11371c738430e9c16299
tamimi30 <mohamad.tamimi6.82@gmail.com>
2026-07-10 22:49:09 +0300

    Remove unused import in SettingsFragment
    Removed unused import for LanguageHelper.

--- app/src/main/java/com/example/oneuiapp/fragment/settings/SettingsFragment.java
+++ app/src/main/java/com/example/oneuiapp/fragment/settings/SettingsFragment.java
@@ -34,7 +34,6 @@ import dev.oneuiproject.oneui.widget.Toast;
 import com.example.oneuiapp.R;
 import com.example.oneuiapp.MyApplication;
 import com.example.oneuiapp.fragment.settings.about.AboutActivity;
-import com.example.oneuiapp.fragment.settings.utils.LanguageHelper;
 import com.example.oneuiapp.fragment.settings.utils.SettingsHelper;
 import com.example.oneuiapp.fragment.settings.viewmodel.SettingsViewModel;
 
