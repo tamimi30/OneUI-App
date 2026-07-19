14757bcc8aa1ef1b90abc4b2adca917913068abc
tamimi30 <mohamad.tamimi6.82@gmail.com>
2026-07-19 06:05:45 +0300

    Change drawer lock behavior in showSearchMode

--- lib/src/main/java/dev/oneuiproject/oneui/layout/DrawerLayout.java
+++ lib/src/main/java/dev/oneuiproject/oneui/layout/DrawerLayout.java
@@ -244,7 +244,7 @@ public class DrawerLayout extends ToolbarLayout {
 
     @Override
     public void showSearchMode() {
-        lockDrawerIfAvailable(true);
+        lockDrawerIfAvailable(false);
         super.showSearchMode();
     }
 
