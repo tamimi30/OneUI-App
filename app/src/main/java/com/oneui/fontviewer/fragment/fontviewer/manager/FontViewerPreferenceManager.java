9badccc92f7abfadaebf610a052f8a88e77b4b79
tamimi30 <mohamad.tamimi6.82@gmail.com>
2026-07-24 06:35:40 +0300

    Import SettingsDataStore in FontViewerPreferenceManager

--- app/src/main/java/com/oneui/fontviewer/fragment/fontviewer/manager/FontViewerPreferenceManager.java
+++ app/src/main/java/com/oneui/fontviewer/fragment/fontviewer/manager/FontViewerPreferenceManager.java
@@ -3,9 +3,9 @@ package com.oneui.fontviewer.fragment.fontviewer.manager;
 import android.content.Context;
 import android.util.Log;
 
-import com.oneui.fontviewer.fragment.settings.datastore.SettingsDataStore;
-
 import io.reactivex.rxjava3.schedulers.Schedulers;
+
+import com.oneui.fontviewer.fragment.settings.datastore.SettingsDataStore;
 
 public class FontViewerPreferenceManager {
     
