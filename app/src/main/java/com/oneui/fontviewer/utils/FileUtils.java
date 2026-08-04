59535ea881930707946eb8b88d9f4bc012562c02
tamimi30 <mohamad.tamimi6.82@gmail.com>
2026-08-04 04:25:52 +0300

    Remove font file utility methods from FileUtils
    Removed methods for checking font files and extracting file names.

--- app/src/main/java/com/oneui/fontviewer/utils/FileUtils.java
+++ app/src/main/java/com/oneui/fontviewer/utils/FileUtils.java
@@ -30,32 +30,5 @@ public class FileUtils {
         return "";
     }
     
-    public static boolean isFontFile(String fileName) {
-        if (fileName == null || fileName.isEmpty()) {
-            return false;
-        }
-        
-        String extension = getExtension(fileName).toLowerCase();
-        return "ttf".equals(extension) || 
-               "otf".equals(extension) || 
-               "ttc".equals(extension);
-    }
     
-    public static String getFileNameFromPath(String fullPath) {
-        if (fullPath == null || fullPath.isEmpty()) {
-            return "";
-        }
-        
-        int lastSlashIndex = fullPath.lastIndexOf('/');
-        if (lastSlashIndex >= 0 && lastSlashIndex < fullPath.length() - 1) {
-            return fullPath.substring(lastSlashIndex + 1);
-        }
-        
-        return fullPath;
-    }
-    
-    public static String getFileNameWithoutExtension(String fullPath) {
-        String fileName = getFileNameFromPath(fullPath);
-        return removeExtension(fileName);
-    }
 }
