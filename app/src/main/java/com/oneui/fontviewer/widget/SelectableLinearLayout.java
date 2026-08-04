d8ee3f86c1a263f7e5807bd1792ac0b756b4c6f8
tamimi30 <mohamad.tamimi6.82@gmail.com>
2026-08-04 07:07:36 +0300

    Update SelectableLinearLayout.java

--- app/src/main/java/com/oneui/fontviewer/widget/SelectableLinearLayout.java
+++ app/src/main/java/com/oneui/fontviewer/widget/SelectableLinearLayout.java
@@ -98,7 +98,7 @@ public class SelectableLinearLayout extends LinearLayout {
         isSelectionMode = mode;
         if (checkMode == CHECK_MODE_CHECKBOX && checkBox != null) {
             
-         /**  // بداية الحل: إضافة استثناء للـ CheckBox لمنع مشكلة التطاير بعد تدوير الشاشة
+            // بداية الحل: إضافة استثناء للـ CheckBox لمنع مشكلة التطاير بعد تدوير الشاشة
             android.transition.TransitionSet transitionSet = new android.transition.TransitionSet();
             transitionSet.setOrdering(android.transition.TransitionSet.ORDERING_TOGETHER);
             transitionSet.setDuration(275);
@@ -114,7 +114,7 @@ public class SelectableLinearLayout extends LinearLayout {
             transitionSet.addTransition(fadeTransition);
 
             android.transition.TransitionManager.beginDelayedTransition(this, transitionSet);
-            // نهاية الحل. */
+            // نهاية الحل.
 
             checkBox.setVisibility(mode ? View.VISIBLE : View.GONE);
         }
