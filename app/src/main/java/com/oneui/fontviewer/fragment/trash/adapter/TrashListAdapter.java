b8145302b462238759b757a82d07b498de7aad23
tamimi30 <mohamad.tamimi6.82@gmail.com>
2026-08-05 04:48:58 +0300

    Remove unused CheckBox import from TrashListAdapter

--- app/src/main/java/com/oneui/fontviewer/fragment/trash/adapter/TrashListAdapter.java
+++ app/src/main/java/com/oneui/fontviewer/fragment/trash/adapter/TrashListAdapter.java
@@ -5,7 +5,6 @@ import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
