460a35e0aa862a172ef91e455d5012cd0b530f88
tamimi30 <mohamad.tamimi6.82@gmail.com>
2026-08-05 04:58:23 +0300

    Remove unused import for SortByItemLayout

--- app/src/main/java/com/oneui/fontviewer/fragment/favorite/FavoriteFontListFragment.java
+++ app/src/main/java/com/oneui/fontviewer/fragment/favorite/FavoriteFontListFragment.java
@@ -39,7 +39,6 @@ import com.oneui.fontviewer.fragment.trash.data.TrashRepository;
 import com.oneui.fontviewer.dialog.FontActionDialogs;
 import com.oneui.fontviewer.data.entity.FontFileInfo;
 import com.oneui.fontviewer.widget.sort.FontSortManager;
-import com.oneui.fontviewer.widget.sort.SortByItemLayout;
 import com.oneui.fontviewer.utils.FontUIStateManager;
 import com.oneui.fontviewer.fragment.localfont.adapter.LocalFontListAdapter;
 import com.oneui.fontviewer.fragment.localfont.data.LocalFontCache;
