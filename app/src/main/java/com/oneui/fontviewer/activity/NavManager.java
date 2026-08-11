869a465d0b8064777d1629be7fbaa9e2b17bb683
tamimi30 <mohamad.tamimi6.82@gmail.com>
2026-08-11 03:07:32 +0300

    Update NavManager.java

--- app/src/main/java/com/oneui/fontviewer/activity/NavManager.java
+++ app/src/main/java/com/oneui/fontviewer/activity/NavManager.java
@@ -105,7 +105,7 @@ public class NavManager {
     public void showFragmentAnimated(AppScreen screen) {
         FragmentManager fm = mHost.getAppFragmentManager();
         FragmentTransaction transaction = fm.beginTransaction();
-        transaction.setCustomAnimations(R.anim.fragment_close_enter, R.anim.fragment_close_exit);
+        transaction.setCustomAnimations(R.anim.note_style_fragment_enter, R.anim.fragment_close_exit);
 
         for (AppScreen s : AppScreen.values()) {
             Fragment frag = mHost.getFragment(s);
