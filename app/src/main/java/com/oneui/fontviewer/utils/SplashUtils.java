ee2a1618b6973cd4c676fe8392c165cdbf8a019a
tamimi30 <mohamad.tamimi6.82@gmail.com>
2026-08-19 20:23:21 +0300

    Update SplashUtils.java

--- app/src/main/java/com/oneui/fontviewer/utils/SplashUtils.java
+++ app/src/main/java/com/oneui/fontviewer/utils/SplashUtils.java
@@ -36,10 +36,10 @@ public class SplashUtils {
                 // التصغير يُطبق على الأيقونة فقط
               //  ObjectAnimator iconScaleX = ObjectAnimator.ofFloat(splashIconView, View.SCALE_X, 1f, 1f);
               //  ObjectAnimator iconScaleY = ObjectAnimator.ofFloat(splashIconView, View.SCALE_Y, 1f, 1f);
-                iconScaleX.setInterpolator(oneEasingInterpolator);
-                iconScaleY.setInterpolator(oneEasingInterpolator);
-                iconScaleX.setDuration(100);
-                iconScaleY.setDuration(100);
+               // iconScaleX.setInterpolator(oneEasingInterpolator);
+              //  iconScaleY.setInterpolator(oneEasingInterpolator);
+              //  iconScaleX.setDuration(100);
+                //iconScaleY.setDuration(100);
 
                 AnimatorSet splashAnimSet = new AnimatorSet();
                 splashAnimSet.playTogether(splashAlpha, iconScaleX, iconScaleY);
