e13857f0778899e584165fb210e23463c47eaffd
tamimi30 <mohamad.tamimi6.82@gmail.com>
2026-08-22 05:11:00 +0300

    Increase thread pool size from 4 to 6

--- app/src/main/java/com/oneui/fontviewer/fragment/localfont/LocalFontListFragment.java
+++ app/src/main/java/com/oneui/fontviewer/fragment/localfont/LocalFontListFragment.java
@@ -198,7 +198,7 @@ public class LocalFontListFragment extends Fragment implements AppBarLayout.OnOf
         setHasOptionsMenu(true);
 
         mMainHandler = new Handler(Looper.getMainLooper());
-        mExecutor    = Executors.newFixedThreadPool(4);
+        mExecutor    = Executors.newFixedThreadPool(6);
 
         initializeViewModels();
         setupViewModelObservers();
