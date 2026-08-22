d6545692984407ebab4f199f3fa732ba0a4ccc1a
tamimi30 <mohamad.tamimi6.82@gmail.com>
2026-08-22 07:41:40 +0300

    Update SDK version check for activity transition

--- app/src/main/java/com/oneui/fontviewer/activity/MainActivity.java
+++ app/src/main/java/com/oneui/fontviewer/activity/MainActivity.java
@@ -82,7 +82,7 @@ public class MainActivity extends BaseActivity
 
         splashScreen.setKeepOnScreenCondition(() -> !isUIReady);
 
-        if (android.os.Build.VERSION.SDK_INT >= 34) {
+        if (android.os.Build.VERSION.SDK_INT >= 33) {
             overrideActivityTransition(OVERRIDE_TRANSITION_OPEN, android.R.anim.fade_in, android.R.anim.fade_out);
         }
 
