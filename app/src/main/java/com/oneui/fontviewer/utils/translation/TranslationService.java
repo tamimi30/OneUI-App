d671fe870747683c7ef0f371337dc648bf9d6155
tamimi30 <mohamad.tamimi6.82@gmail.com>
2026-08-23 08:15:57 +0300

    Remove blank line in TranslationService.java
    Removed unnecessary blank line in TranslationService.java

--- app/src/main/java/com/oneui/fontviewer/utils/translation/TranslationService.java
+++ app/src/main/java/com/oneui/fontviewer/utils/translation/TranslationService.java
@@ -1,7 +1,6 @@
 package com.oneui.fontviewer.utils.translation;
 
 import android.content.Context;
-
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.util.Log;
