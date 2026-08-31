5c88684ea06a52063918915049f8dff37f670aab
tamimi30 <mohamad.tamimi6.82@gmail.com>
2026-08-31 15:33:02 +0300

    Increase maximum font size limit to 520

--- app/src/main/java/com/oneui/fontviewer/fragment/fontviewer/FontViewerFragment.java
+++ app/src/main/java/com/oneui/fontviewer/fragment/fontviewer/FontViewerFragment.java
@@ -61,7 +61,7 @@ public class FontViewerFragment extends Fragment {
 
     private static final float DEFAULT_FONT_SIZE   = 34f;
     private static final float MIN_FONT_SIZE       = 12f;
-    private static final float MAX_FONT_SIZE       = 200f;
+    private static final float MAX_FONT_SIZE       = 520f;
     private static final float DEFAULT_FONT_WEIGHT = 400f;
 
     private static float sSessionFontSize = -1f;
