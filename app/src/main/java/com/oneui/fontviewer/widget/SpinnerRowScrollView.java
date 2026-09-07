f6dc5810bb269ec13c25efbcaf54d16c24ad81a0
tamimi30 <mohamad.tamimi6.82@gmail.com>
2026-09-07 08:50:50 +0300

    Increase VERTICAL_RELEASE_SLOP_DP from 20 to 40

--- app/src/main/java/com/oneui/fontviewer/widget/SpinnerRowScrollView.java
+++ app/src/main/java/com/oneui/fontviewer/widget/SpinnerRowScrollView.java
@@ -9,7 +9,7 @@ import android.widget.HorizontalScrollView;
 public class SpinnerRowScrollView extends HorizontalScrollView {
 
     private static final float MICRO_SLOP_DP = 3.5f;
-    private static final float VERTICAL_RELEASE_SLOP_DP = 20f;
+    private static final float VERTICAL_RELEASE_SLOP_DP = 40f;
 
     private final float microSlopPx;
     private final float verticalReleaseSlopPx;
